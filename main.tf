91b38b890cba80c6df1955f6396feea3868bce3c
sindhuja gutha <sindhugutha88@gmail.com>
2023-10-04 11:49:50 -0500

    the commits are done properly

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "main"  {
   cidr_block = "var.cidr"
 }
 module "subnets" {
-  source = "subnets"
+  source = "./subnets"
   for_each = var.subnets
   subnets = each.value
   vpc_id = aws_vpc.main.id
