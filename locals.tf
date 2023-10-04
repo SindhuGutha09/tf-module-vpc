6b28d5bb0de8ca28347bd4c0309c69b6de14b345
sindhuja gutha <sindhugutha88@gmail.com>
2023-10-04 15:05:30 -0500

    the commits are done properly

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
   #subnets
   public_subnet_ids    = [for k,v in lookup(lookup(module.subnets, "public", null), "subnet_ids", null) : v.id]
-  app_subnet_ids       = [for k,v in lookup(lookup(module.subnets, "app", null), "subnet_ids", null) : v.id]
-  db_subnet_ids        = [for k,v in lookup(lookup(module.subnets, "db", null), "subnet_ids", null) : v.id]
+  app_subnet_ids       = [for k,v in lookup(lookup(module.subnets, "app", null), "subnet_ids", null) : v.id if v.id != null]
+  db_subnet_ids = [for k, v in lookup(lookup(module.subnets, "db", null), "subnet_ids", null) :v.id if v.id != null]
   private_subnet_ids   = concat(local.app_subnet_ids, local.db_subnet_ids)
 
   #Route Table
